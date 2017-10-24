3980a726fc7b31f43bc1c56a2eab11f35fa07ae3
= <=>
2017-10-23 21:49:02 -0400

    Add tests for linked list and fix bug that prevented reassignement of tail reference

--- tests/ll_test.c
+++ tests/ll_test.c
@@ -41,26 +41,111 @@ START_TEST(list_pq_basic){
   struct list_node *mk;
 
   ll_pq_enqueue(&l, (void*)&priorities[0], priorities[0]);
+  ck_assert_int_eq(l.size, 1);
   ll_pq_enqueue(&l, (void*)&priorities[2], priorities[2]);
+  ck_assert_int_eq(l.size, 2);
   ll_pq_enqueue(&l, (void*)&priorities[1], priorities[1]);
+  ck_assert_int_eq(l.size, 3);
   ll_pq_enqueue(&l, (void*)&priorities[0], priorities[0]);
+  ck_assert_int_eq(l.size, 4);
   ll_pq_enqueue(&l, (void*)&priorities[3], priorities[3]);
+  ck_assert_int_eq(l.size, 5);
   ll_pq_enqueue(&l, (void*)&priorities[3], priorities[3]);
+  ck_assert_int_eq(l.size, 6);
   ll_pq_enqueue(&l, (void*)&priorities[1], priorities[1]);
+  ck_assert_int_eq(l.size, 7);
   ll_pq_enqueue(&l, (void*)&priorities[2], priorities[2]);
+  ck_assert_int_eq(l.size, 8);
 
   
   ck_assert_int_eq(0, *(int*)ll_pq_dequeue(&l));
+  ck_assert_int_eq(l.size, 7);
   ck_assert_int_eq(0, *(int*)ll_pq_dequeue(&l));
+  ck_assert_int_eq(l.size, 6);
   ck_assert_int_eq(1, *(int*)ll_pq_dequeue(&l));
+  ck_assert_int_eq(l.size, 5);
   ck_assert_int_eq(1, *(int*)ll_pq_dequeue(&l));
+  ck_assert_int_eq(l.size, 4);
   ck_assert_int_eq(2, *(int*)ll_pq_dequeue(&l));
+  ck_assert_int_eq(l.size, 3);
   ck_assert_int_eq(2, *(int*)ll_pq_dequeue(&l));
+  ck_assert_int_eq(l.size, 2);
   ck_assert_int_eq(3, *(int*)ll_pq_dequeue(&l));
+  ck_assert_int_eq(l.size, 1);
   ck_assert_int_eq(3, *(int*)ll_pq_dequeue(&l));
-
+  ck_assert_int_eq(l.size, 0);
 
   ck_assert_ptr_eq(NULL, ll_pq_dequeue(&l));
+
+  
+  ll_pq_enqueue(&l, (void*)&priorities[0], priorities[0]);
+  ck_assert_int_eq(l.size, 1);
+  ll_pq_enqueue(&l, (void*)&priorities[2], priorities[2]);
+  ck_assert_int_eq(l.size, 2);
+  ll_pq_enqueue(&l, (void*)&priorities[1], priorities[1]);
+  ck_assert_int_eq(l.size, 3);
+  ll_pq_enqueue(&l, (void*)&priorities[0], priorities[0]);
+  ck_assert_int_eq(l.size, 4);
+  ll_pq_enqueue(&l, (void*)&priorities[3], priorities[3]);
+  ck_assert_int_eq(l.size, 5);
+  ll_pq_enqueue(&l, (void*)&priorities[3], priorities[3]);
+  ck_assert_int_eq(l.size, 6);
+  ll_pq_enqueue(&l, (void*)&priorities[1], priorities[1]);
+  ck_assert_int_eq(l.size, 7);
+  ll_pq_enqueue(&l, (void*)&priorities[2], priorities[2]);
+  ck_assert_int_eq(l.size, 8);
+
+  
+  ck_assert_int_eq(0, *(int*)ll_pq_dequeue(&l));
+  ck_assert_int_eq(l.size, 7);
+  ck_assert_int_eq(0, *(int*)ll_pq_dequeue(&l));
+  ck_assert_int_eq(l.size, 6);
+  ck_assert_int_eq(1, *(int*)ll_pq_dequeue(&l));
+  ck_assert_int_eq(l.size, 5);
+  ck_assert_int_eq(1, *(int*)ll_pq_dequeue(&l));
+  ck_assert_int_eq(l.size, 4);
+  ck_assert_int_eq(2, *(int*)ll_pq_dequeue(&l));
+  ck_assert_int_eq(l.size, 3);
+  ck_assert_int_eq(2, *(int*)ll_pq_dequeue(&l));
+  ck_assert_int_eq(l.size, 2);
+  ck_assert_int_eq(3, *(int*)ll_pq_dequeue(&l));
+  ck_assert_int_eq(l.size, 1);
+  ck_assert_int_eq(3, *(int*)ll_pq_dequeue(&l));
+  ck_assert_int_eq(l.size, 0);
+
+ 
+
+}
+END_TEST
+
+
+START_TEST(queue_usage){
+  struct list l;
+
+  ll_initialize(&l);
+
+  int priorities[6] = {0, 1, 2, 3, 4, 5};
+  
+  ll_pq_enqueue(&l, (void*)&priorities[0], priorities[0]);
+  ck_assert_int_eq(l.size, 1);
+  ll_pq_enqueue(&l, (void*)&priorities[0], priorities[0]);
+  ck_assert_int_eq(l.size, 2);
+  
+
+  ck_assert_int_eq(0, *(int*)ll_pq_dequeue(&l));
+  ck_assert_int_eq(l.size, 1);
+  
+  ll_pq_enqueue(&l, (void*)&priorities[0], priorities[0]);
+  ck_assert_int_eq(l.size, 2);
+
+  ck_assert_int_eq(0, *(int*)ll_pq_dequeue(&l));
+  ck_assert_int_eq(l.size, 1);
+
+  ck_assert_int_eq(0, *(int*)ll_pq_dequeue(&l));
+  ck_assert_int_eq(l.size, 0);
+
+  ck_assert_ptr_eq(NULL, ll_pq_dequeue(&l));
+
 }
 END_TEST
 
@@ -76,6 +161,7 @@ Suite * list_suite(void){
 
   tcase_add_test(tc_core, list_insert_and_remove);
   tcase_add_test(tc_core, list_pq_basic);
+  tcase_add_test(tc_core, queue_usage);
   suite_add_tcase(s, tc_core);
 
   return s;
